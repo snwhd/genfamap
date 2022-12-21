c102d5ee751a7ce2838501a286e63ba789181f41
snwhd <none@none>
2022-12-21 04:27:01 +0000

    0.108

--- crawler/scrape.py
+++ crawler/scrape.py
@@ -16,7 +16,7 @@ import os
 
 
 CACHE_DIR = 'files'
-CACHE_VERSION = '0.104'
+CACHE_VERSION = '0.108'
 STATIC_ENDPOINT = f'https://genfanad-static.s3.us-east-2.amazonaws.com/versioned/{CACHE_VERSION}/data_client'
 IMAGES_DIR = 'img'
 HTML_DIR = 'html'
